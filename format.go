d77230b809cbdd45e56aa9beb3d368aa23c0ab0b
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2013-02-07 16:29:23 +0100

    Fixed exported format wrappers.

--- format.go
+++ format.go
@@ -6,11 +6,11 @@ import (
 )
 
 func Format(parts ...interface{}) (string, error) {
-	return format(true, parts)
+	return format(true, parts...)
 }
 
 func FormatWithoutReset(parts ...interface{}) (string, error) {
-	return format(false, parts)
+	return format(false, parts...)
 }
 
 func format(reset bool, parts ...interface{}) (string, error) {
@@ -18,26 +18,28 @@ func format(reset bool, parts ...interface{}) (string, error) {
 
 	for i, part := range parts {
 		// Just a simple piece of string to add to the result
-		str, ok := part.(string)
-		if ok {
+		str, strOk := part.(string)
+		if strOk {
 			b.WriteString(str)
 			continue
 		}
 
 		// Add a SGR definition
-		sgr, ok := part.(Sgr)
-		if ok {
+		sgr, sgrOk := part.(Sgr)
+		if sgrOk {
 			b.Write(CSI)
 			b.Write(sgr.Sequence())
 			b.WriteByte(SgrEnd)
 			continue
 		}
 
-		return "", fmt.Errorf("Invalid part at position %d.", i)
+		return "", fmt.Errorf("Invalid part at position %d. Expecting type `string` or `tcolor.Sgr`.", i)
 	}
 
 	if reset {
+		b.Write(CSI)
 		b.Write(Reset.Sequence())
+		b.WriteByte(SgrEnd)
 	}
 	return b.String(), nil
 }
