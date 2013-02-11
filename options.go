9a635e6ce3085bfb78428456bdfc89ce6e69d0c1
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2013-02-11 08:42:52 +0100

    Renamed `basicSgr` to `sgrOption`.

--- options.go
+++ options.go
@@ -5,37 +5,37 @@ import (
 )
 
 // Simple code like reset and bold
-type basicSgr uint8
+type sgrOption uint8
 
-func (s basicSgr) code() uint8 {
+func (s sgrOption) code() uint8 {
 	return uint8(s)
 }
 
-func (s basicSgr) sequence() []byte {
+func (s sgrOption) sequence() []byte {
 	return []byte(strconv.Itoa(int(s)))
 }
 
 const (
-	Reset                = basicSgr(0)  // Reset all SGR options.
-	ResetForegroundColor = basicSgr(39) // Reset foreground color.
-	ResetBackgroundColor = basicSgr(49) // Reset background color.
+	Reset                = sgrOption(0)  // Reset all SGR options.
+	ResetForegroundColor = sgrOption(39) // Reset foreground color.
+	ResetBackgroundColor = sgrOption(49) // Reset background color.
 
-	Bold    = basicSgr(1)
-	BoldOff = basicSgr(22)
+	Bold    = sgrOption(1)
+	BoldOff = sgrOption(22)
 
-	Underline    = basicSgr(4)
-	UnderlineOff = basicSgr(24)
+	Underline    = sgrOption(4)
+	UnderlineOff = sgrOption(24)
 
-	Blink    = basicSgr(5) // Less then 150 per minute.
-	BlinkOff = basicSgr(25)
+	Blink    = sgrOption(5) // Less then 150 per minute.
+	BlinkOff = sgrOption(25)
 
-	ImageNegative = basicSgr(7)  // Set reversed-video active (foreground and background negative).
-	ImagePositive = basicSgr(27) // Reset reversed-video to normal.
+	ImageNegative = sgrOption(7)  // Set reversed-video active (foreground and background negative).
+	ImagePositive = sgrOption(27) // Reset reversed-video to normal.
 
-	Framed             = basicSgr(51)
-	Encircled          = basicSgr(52)
-	FramedEncircledOff = basicSgr(54)
+	Framed             = sgrOption(51)
+	Encircled          = sgrOption(52)
+	FramedEncircledOff = sgrOption(54)
 
-	Overlined    = basicSgr(53)
-	OverlinedOff = basicSgr(55)
+	Overlined    = sgrOption(53)
+	OverlinedOff = sgrOption(55)
 )
