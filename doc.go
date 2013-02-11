636b3078a3ba2ecc780495fa53ba8792cf14bac3
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2013-02-11 14:46:43 +0100

    Updated doc.go

--- doc.go
+++ doc.go
@@ -1,22 +1,23 @@
+/*
+Typical usage is to use Parseln() or MustParseln(), and use the resulting string in succeeding `fmt` or `log` calls.
+
+```go
+package main
+
+import (
+	"fmt"
+	"github.com/foize/go.sgr"
+)
+
+func main() {
+	exampleString := sgr.MustParseln("This is an example: [fg-red bold] important text [reset] normal text again.")
+	fmt.Print(exampleString)
+
+	secretNumberFormat := sgr.MustParseln("The secret number is [bg-17 blink]%d")
+	fmt.Printf(secretNumberFormat, 42)
+
+	fmt.Println("This text is normal again, MustParseln puts a reset at the end of the line.")
+}
+```
+*/
 package sgr
-
-// Typical usage is to use Parseln() or MustParseln(), and use the resulting string in succeeding `fmt` or `log` calls.
-
-// ```go
-// package main
-
-// import (
-// 	"fmt"
-// 	"github.com/foize/go.sgr"
-// )
-
-// func main() {
-// 	exampleString := sgr.MustParseln("This is an example: [fg-red bold] important text [reset] normal text again.")
-// 	fmt.Print(exampleString)
-
-// 	secretNumberFormat := sgr.MustParseln("The secret number is [bg-17 blink]%d")
-// 	fmt.Printf(secretNumberFormat, 42)
-
-// 	fmt.Println("This text is normal again, MustParseln puts a reset at the end of the line.")
-// }
-// ```
