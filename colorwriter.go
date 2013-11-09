b45085fca21a2f92bb319c5d77fe0f88e74c2706
GeertJohan <gjr19912@gmail.com>
2013-11-09 15:07:39 +0100

    checking all writes. renamed ColorWriter's field w to writer returning length of originally passed in bytes

--- colorwriter.go
+++ colorwriter.go
@@ -15,7 +15,7 @@ var resetBytes = []byte(Reset)
 
 // ColorWriter implements io.Writer
 type ColorWriter struct {
-	w            io.Writer
+	writer       io.Writer
 	colorBytes   []byte
 	forceNewline bool
 }
@@ -24,7 +24,7 @@ type ColorWriter struct {
 // example: NewColorWriter(os.Stdout, sgr.FgBlue, true)
 func NewColorWriter(w io.Writer, color string, forceNewline bool) *ColorWriter {
 	return &ColorWriter{
-		w:            w,
+		writer:       w,
 		colorBytes:   []byte(color),
 		forceNewline: forceNewline,
 	}
@@ -33,25 +33,50 @@ func NewColorWriter(w io.Writer, color string, forceNewline bool) *ColorWriter {
 // Write writes the bytes to underlying writer, but adds coloring
 func (cw *ColorWriter) Write(p []byte) (n int, err error) {
 	// add colors
-	cw.w.Write(cw.colorBytes)
+	_, err = cw.writer.Write(cw.colorBytes)
+	if err != nil {
+		return
+	}
 
 	// write tekst/data
-	cw.w.Write(p[:len(p)-2])
+	_, err = cw.writer.Write(p[:len(p)-2])
+	if err != nil {
+		return
+	}
 
 	// force newline if wanted && required
 	if cw.forceNewline {
 		if p[len(p)-1] == '\n' {
-			cw.w.Write(ExistingNewline)
-			cw.w.Write(newlineBytes)
+			_, err = cw.writer.Write(ExistingNewline)
+			if err != nil {
+				return
+			}
+
+			_, err = cw.writer.Write(newlineBytes)
+			if err != nil {
+				return
+			}
 		} else {
-			cw.w.Write(p[len(p)-1:])
-			cw.w.Write(ForcedNewline)
-			cw.w.Write(newlineBytes)
+			_, err = cw.writer.Write(p[len(p)-1:])
+			if err != nil {
+				return
+			}
+
+			_, err = cw.writer.Write(ForcedNewline)
+			if err != nil {
+				return
+			}
+
+			_, err = cw.writer.Write(newlineBytes)
+			if err != nil {
+				return
+			}
 		}
 	}
-	cw.w.Write(resetBytes)
+	_, err = cw.writer.Write(resetBytes)
+	if err != nil {
+		return
+	}
 
-	//++ TODO: proper return.
-	//++ should this return the amount of p bytes that where written? or also the sgr bytes?
 	return len(p), nil
 }
